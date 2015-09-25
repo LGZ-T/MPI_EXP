e2050b3d7b247a9ba6afa58551293db4dd984bea
haomeng <haomeng1@yeah.net>
2015-09-25 13:38:58 +0800

    add the precision of the mpi_hello

--- src/mpi_hello.cpp
+++ src/mpi_hello.cpp
@@ -4,6 +4,7 @@
 #include <mpi.h>
 #include <iostream>
 #include <unistd.h>
+#include <iomanip>
 #include <time.h>
 using namespace std;
 
@@ -16,7 +17,7 @@ void data_init(int * send_or_recv, int size)
 }
 void clean_cache(int flag, int size)
 {
-   for(int i = 0;i < 20;i++)
+   for(int i = 0;i < 5;i++)
    {
       int * sending = new int[size];
       int* recving = new int[size];
@@ -82,19 +83,26 @@ int main(int argc ,char* argv[])
          data_init(sending, size);
 
 
+         clean_cache(my_rank, size);
+
          MPI_Barrier(MPI_COMM_WORLD);
          //sleep(0.1);
          starttime = MPI_Wtime();
          MPI_Send(sending,size,MPI_INT,1,i,MPI_COMM_WORLD);
          endtime = MPI_Wtime();
-         cout <<"Os\t"<< (endtime-starttime) <<"\ts\n";
+         cout << fixed << setprecision(15);
+         cout <<"t1_Os\t"<< (endtime-starttime) <<"\ts\n";
 
          delete[] sending;
+
+         clean_cache(my_rank, size);
 
          double recvtime;
          MPI_Barrier(MPI_COMM_WORLD);
          MPI_Recv(&recvtime,1,MPI_DOUBLE,1,i,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-         cout << "Os+(S-1)G+L+Or\t"<< recvtime<< "\ts\n";
+         cout << "t1_Os+(S-1)G+L+Or\t"<< recvtime<< "\ts\n";
+
+         clean_cache(my_rank, size);
 
          double endtime_Or;
          MPI_Barrier(MPI_COMM_WORLD);
@@ -104,8 +112,10 @@ int main(int argc ,char* argv[])
 
          MPI_Recv(recving,size,MPI_INT,1,i,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
          endtime_Or = MPI_Wtime();
-         cout << "Os+(S-1)*G+L\t" << (endtime - starttime) << "\ts\n";
-         cout << "Or?\t"<< (endtime_Or - endtime) << "\ts\n";
+         cout << "t2_Os+(S-1)*G+L\t" << (endtime - starttime) << "\ts\n";
+         cout << "t2_Or?\t"<< (endtime_Or - endtime) << "\ts\n";
+         MPI_Recv(&recvtime,1,MPI_DOUBLE,1,i,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+         cout << "t2_Os\t" << recvtime << "\ts\n";
 
 
          delete[] recving;
@@ -113,13 +123,15 @@ int main(int argc ,char* argv[])
          recving = new int[size];
          data_init(recving, size);
 
+         clean_cache(my_rank, size);
+
          MPI_Barrier(MPI_COMM_WORLD);
          MPI_Probe(1,i,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
          sleep(0.1);
          starttime = MPI_Wtime();
          MPI_Recv(recving,size,MPI_INT,1,i,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
          endtime = MPI_Wtime();
-         cout << "Or\t"<< (endtime - starttime) << "\ts\n";
+         cout << "t3_Or\t"<< (endtime - starttime) << "\ts\n\n";
          delete[] recving;
       }
       else
@@ -128,10 +140,14 @@ int main(int argc ,char* argv[])
          int* recving = new int[size];
          data_init(sending, size);
 
+         clean_cache(my_rank, size);
+
          MPI_Barrier(MPI_COMM_WORLD);
          starttime = MPI_Wtime();
          MPI_Recv(recving,size,MPI_INT,0,i,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
          endtime = MPI_Wtime();
+
+         clean_cache(my_rank, size);
 
          double recvtime = endtime - starttime;
          MPI_Barrier(MPI_COMM_WORLD);
@@ -140,14 +156,20 @@ int main(int argc ,char* argv[])
 
          delete[] recving;
 
+         clean_cache(my_rank, size);
+
          MPI_Barrier(MPI_COMM_WORLD);
          starttime = MPI_Wtime(); 
          MPI_Send(sending,size,MPI_INT,0,i,MPI_COMM_WORLD);
          endtime = MPI_Wtime();
+         recvtime = endtime - starttime;
+         MPI_Send(&recvtime,1,MPI_DOUBLE,0,i,MPI_COMM_WORLD);
 
          delete[] sending;
          sending  = new int[size];
          data_init(sending, size);
+
+         clean_cache(my_rank, size);
 
          MPI_Barrier(MPI_COMM_WORLD);
          MPI_Send(sending,size,MPI_INT,0,i,MPI_COMM_WORLD);
